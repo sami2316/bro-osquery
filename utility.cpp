02bdc56b9f2ce5ecfab1f8cecb495b4939c20eaf
Sami Ullah <sami2316@users.noreply.github.com>
2019-11-05 10:01:29 +0900

    Update utility.cpp

--- utility.cpp
+++ utility.cpp
@@ -1,12 +1,4 @@
-/* 
- *  Copyright (c) 2015, Next Generation Intelligent Networks (nextGIN), RC.
- *  Institute of Space Technology
- *  All rights reserved.
- * 
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant
- *  of patent rights can be found in the PATENTS file in the same directory.
- */
+
 #include <signal.h>
 #include <errno.h>
 #include "utility.h"
