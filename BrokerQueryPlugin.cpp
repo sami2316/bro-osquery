278cadf04c08c1d572c87527e10b7146d542d9df
Sami Ullah <sami2316@users.noreply.github.com>
2019-11-05 09:59:42 +0900

    Update BrokerQueryPlugin.cpp

--- BrokerQueryPlugin.cpp
+++ BrokerQueryPlugin.cpp
@@ -1,13 +1,3 @@
-/* 
- *  Copyright (c) 2015, Next Generation Intelligent Networks (nextGIN), RC.
- *  Institute of Space Technology
- *  All rights reserved.
- * 
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
- */
 
 #include "BrokerQueryPlugin.h"
 
